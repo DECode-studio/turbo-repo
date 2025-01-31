b319b923d6e07f5fa5873ce207ff8106bdd09dd0
user <user@users-MacBook-Pro.local>
2025-01-31 15:48:58 +0700

    fix turbo repo config

--- apps/frontend-repo/src/components/UpdateButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Button } from '@mui/material';
-const UpdateButton = ({ onClick }) => {
-    return (<Button variant="contained" color="secondary" onClick={onClick}>
-      Update
-    </Button>);
-};
-export default UpdateButton;
--- apps/frontend-repo/src/service/config/firebaseConfig.js
+++
@@ -1,6 +0,0 @@
-import admin from 'firebase-admin';
-const serviceAccount = require('../../../firebase-config.json');
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-});
-export const db = admin.firestore();
